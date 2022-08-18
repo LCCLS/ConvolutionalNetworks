558421ab5687056bc168313255ceb9fa539cd76c
Leo Classon <leoclasson@Leos-MacBook-Pro.local>
2022-08-18 17:06:06 +0200

    main script

--- CovidClassification/main.py
+++ CovidClassification/main.py
@@ -6,33 +6,34 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.metrics import SparseCategoricalAccuracy, AUC
 
-from utils import data_augmentation, plot_performance
+from utils import *
 from NN_model import build_model
 
 # defining basic hyperparameters
-OPTIMIZER = Adam(learning_rate=0.005)
-LOSS_FUNCTION = SparseCategoricalCrossentropy()
-EVAL_METRIC = SparseCategoricalAccuracy()
-BATCH_SIZE = 5
+OPTIMIZER = Adam(learning_rate=0.008)
+EPOCHS = 60
+TRAINING_BATCH_SIZE = 32
+VAL_BATCH_SIZE = 12
 
 # loading the datasets and augmenting them based on the function in utils.py
-training_iterator = data_augmentation('Covid19-dataset/train')
-validation_iterator = data_augmentation('Covid19-dataset/test')
-sample_batch_input, sample_batch_label = training_iterator.next()
+training_iterator = data_augmentation('Covid19-dataset/train', TRAINING_BATCH_SIZE)
+validation_iterator = data_loader('Covid19-dataset/test', VAL_BATCH_SIZE)
 
 # defining the model based on the build_model() function imported from NN_model.py
 XRay_Classifier = build_model()
-XRay_Classifier.compile(optimizer=OPTIMIZER, loss='sparse_categorical_crossentropy',
+XRay_Classifier.compile(optimizer=OPTIMIZER,
+                        loss='sparse_categorical_crossentropy',
                         metrics=['accuracy'])
 # print(XRay_Classifier.summary())
 
 # defining the early_stop callback function to be passed into the .fit() function and training the model
-early_stop = tensorflow.keras.callbacks.EarlyStopping(monitor='loss', patience=5)
+early_stop = tensorflow.keras.callbacks.EarlyStopping(monitor='loss',
+                                                      patience=5)
 history = XRay_Classifier.fit(training_iterator,
-                              steps_per_epoch=training_iterator.samples / BATCH_SIZE,
-                              epochs=60,
+                              steps_per_epoch=training_iterator.samples / TRAINING_BATCH_SIZE,
+                              epochs=EPOCHS,
                               validation_data=validation_iterator,
-                              validation_steps=validation_iterator.samples / BATCH_SIZE,
+                              validation_steps=validation_iterator.samples / VAL_BATCH_SIZE,
                               callbacks=[early_stop])
 
 plot_performance(history, 'accuracy')
@@ -42,11 +43,12 @@ score = XRay_Classifier.evaluate(validation_iterator, verbose=0)
 print(f'Test loss: {score[0]}')
 print(f'Test accuracy: {score[1]}')
 
-y_pred = XRay_Classifier.predict(validation_iterator.samples)
-y_pred = np.argmax(y_pred, axis=1)
-class_names = ['Spruce/Fir', 'Lodgepole Pine',
-               'Ponderosa Pine', 'Cottonwood/Willow',
-               'Aspen', 'Douglas-fir', 'Krummholz']
+X_test, y_test = validation_iterator.next()
 
-print(classification_report(validation_iterator.labels, y_pred, target_names=class_names))
-plot_heatmap(class_names, y_pred, validation_iterator)
+y_pred = XRay_Classifier.predict(X_test, steps=validation_iterator.samples/VAL_BATCH_SIZE)
+y_pred = np.argmax(y_pred, axis=1)
+true_classes = validation_iterator.classes
+class_labels = list(validation_iterator.class_indices.keys())
+
+print(classification_report(y_test, y_pred, target_names=class_labels))
+plot_heatmap(class_labels, y_pred, y_test)
